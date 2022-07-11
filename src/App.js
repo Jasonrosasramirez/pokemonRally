e4ec8235b11573fe7bd21fedf5b5cc92938cc095
Jason <jrosasrjf@gmail.com>
2022-07-10 23:07:56 -0500

    conditioning

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // {import something specific}
+
+// import CSS Styles 
+
+
+// Importing page files 
+
+
+// Importing page components  <Route path = "/PageTemplate" component = {PageTemplate} />
+
+
+function App() {
+  
+  // The return can only export 1 element. You can bundle multiple elements using a cluster
+  return (
+    null
+  );
+}
+
+export default App; // I export to the screen. This gets sent to index.js
+// without exporting the APP, nothing gets displayed on screen. 
